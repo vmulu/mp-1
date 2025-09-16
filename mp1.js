d9babdb3e968ee7e9505822e7b623a23ea265175
vmulu <vmulu@bu.edu>
2025-09-16 17:49:39 -0400

    update

--- mp1.js
+++ mp1.js
@@ -7,3 +7,29 @@ let output = document.getElementById("output")
 function doAdd(){
     output.innerHTML=String(Number(one.value)+Number(two.value));
 }
+
+function doSub(){
+    output.innerHTML=String(Number(one.value)-Number(two.value));
+}
+
+function doMul(){
+    output.innerHTML=String(Number(one.value)*Number(two.value));
+}
+
+function doDiv(){
+    output.innerHTML=String(Number(one.value)/Number(two.value));
+}
+
+function doPow(){
+    x = 1
+    for(let i=0; i<Number(two.value); i++){
+        x *= Number(one.value)
+    }
+    output.innerHTML=String(x);
+}
+
+function doClear(){
+    document.getElementById('one').value = '';
+    document.getElementById('two').value = '';
+    output.innerHTML='';
+}
